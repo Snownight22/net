c18794fef21e52f9a438986ff32a0f8d1229b3c5
root <root@testmachine.(none)>
2018-07-13 16:31:24 +0800

    [update]:更新log库

--- include/log.h
+++ include/log.h
@@ -21,19 +21,19 @@ void log_core_printf(eLogLevel level, const char* format, ...);
 
 /*各级别日志对外宏*/
 #define LOG_DEBUG(fmt, ...) \
-    log_core_printf(LOG_DEBUG, "%s:%s(%d):"fmt, filename(__FILE__), __FUNCTION__, __LINE__, ##__VA_ARGS__);
+    log_core_printf(LOG_DEBUG, "%s(%d):%s:"fmt, filename(__FILE__), __LINE__, __FUNCTION__, ##__VA_ARGS__);
 
 #define LOG_INFO(fmt, ...) \
-    log_core_printf(LOG_INFO, "%s:%s(%d):"fmt, filename(__FILE__), __FUNCTION__, __LINE__, ##__VA_ARGS__);
+    log_core_printf(LOG_INFO, "%s(%d):%s:"fmt, filename(__FILE__), __LINE__, __FUNCTION__, ##__VA_ARGS__);
 
 #define LOG_WARNING(fmt, ...) \
-    log_core_printf(LOG_WARNING, "%s:%s(%d):"fmt, filename(__FILE__), __FUNCTION__, __LINE__, ##__VA_ARGS__);
+    log_core_printf(LOG_WARNING, "%s(%d):%s:"fmt, filename(__FILE__), __LINE__, __FUNCTION__, ##__VA_ARGS__);
 
 #define LOG_ERROR(fmt, ...) \
-    log_core_printf(LOG_ERROR, "%s:%s(%d):"fmt, filename(__FILE__), __FUNCTION__, __LINE__, ##__VA_ARGS__);
+    log_core_printf(LOG_ERROR, "%s(%d):%s:"fmt, filename(__FILE__), __LINE__, __FUNCTION__, ##__VA_ARGS__);
 
 #define LOG_CRITICAL(fmt, ...) \
-    log_core_printf(LOG_CRITICAL, "%s:%s(%d):"fmt, filename(__FILE__), __FUNCTION__, __LINE__, ##__VA_ARGS__);
+    log_core_printf(LOG_CRITICAL, "%s(%d):%s:"fmt, filename(__FILE__), __LINE__, __FUNCTION__, ##__VA_ARGS__);
 
 /*统一对外接口*/
 void log_init(char *config_file);
